cabc14464a7a179cb56f6c21af8488bbda77ae25
yousoumar <oumaiyr@gmail.com>
2021-12-01 11:57:01 +0100

    codes hamburger menu

--- src/javaScript/app.js
+++ src/javaScript/app.js
@@ -1,1 +1,6 @@
 import "../sass/app.scss";
+const header = document.querySelector(".header");
+const hamburger = header.querySelector(".header .hamburger");
+hamburger.addEventListener("click", () => {
+  header.classList.toggle("active");
+});
